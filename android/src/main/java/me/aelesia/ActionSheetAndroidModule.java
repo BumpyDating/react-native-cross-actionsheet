817c0941a44e47e43f83fbe8a33bdb724d3b4632
Valentyn Berehovyi <valentyn.berehovyi@gmail.com>
2024-11-13 16:13:36 +0200

    Update ActionSheetAndroidModule.java

--- android/src/main/java/me/aelesia/ActionSheetAndroidModule.java
+++ android/src/main/java/me/aelesia/ActionSheetAndroidModule.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -80,7 +81,7 @@ public class ActionSheetAndroidModule extends ReactContextBaseJavaModule {
             if (title != null || message != null) {
                 View border = new View(reactContext);
                 border.setBackgroundColor(Color.parseColor("#DDDDDD"));
-                border.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
+                border.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, 1));
                 listView.addHeaderView(border);
             }
             ActionSheetListAdapter adapter = new ActionSheetListAdapter(reactContext, strList, destructiveIndex, tintColor, position -> {
